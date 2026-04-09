c0a3d49db4cbeaf58895a27881b15e6334cddae2
KaeligBrl <berelkaelig35@gmail.com>
2026-04-09 22:49:00 +0200

    feat: téléchargement PDF quittances depuis l'espace locataire

--- src/Controller/TenantRentReceiptController.php
+++ src/Controller/TenantRentReceiptController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\RentReceipt;
 use App\Entity\Tenant;
 use App\Repository\RentReceiptRepository;
+use App\Service\RentReceiptPdfService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,6 +13,10 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/espace-locataire/quittances', name: 'tenant_quittance_')]
 class TenantRentReceiptController extends AbstractController
 {
+    public function __construct(
+        private RentReceiptPdfService $pdfService,
+    ) {}
+
     #[Route('', name: 'index', methods: ['GET'])]
     public function index(RentReceiptRepository $repo): Response
     {
@@ -47,6 +52,26 @@ class TenantRentReceiptController extends AbstractController
         return $this->render('tenant/quittances_show.html.twig', [
             'tenant'  => $tenant,
             'receipt' => $receipt,
+        ]);
+    }
+
+    #[Route('/{id}/pdf', name: 'pdf', methods: ['GET'])]
+    public function pdf(RentReceipt $receipt): Response
+    {
+        $tenant = $this->getUser();
+        if (!$tenant instanceof Tenant) {
+            return $this->redirectToRoute('homepage');
+        }
+
+        if ($receipt->getTenant()?->getId() !== $tenant->getId()) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $content = $this->pdfService->generate($receipt);
+
+        return new Response($content, 200, [
+            'Content-Type'        => 'application/pdf',
+            'Content-Disposition' => 'inline; filename="quittance-'.$receipt->getNumber().'.pdf"',
         ]);
     }
 }
